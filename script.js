3ed00b22837449010e952298dfaf9845fa0db2b2
Davidmitseruk <davidmitseruk@gmail.com>
2024-05-20 14:31:35 +0200

    Hw 2

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+//1
 const c = 3;
 const f = 2 * c + 32;
 console.log(f);
 
+//2
+const month = 30;
+console.log(month * 24); // 720 hours
+console.log(720 * 60); // 43200 minutes
+
+//3 
+const health = 100;
+const energy = 100;
+console.log(health + energy - 128);
+
+//4
+const item = 140;
+console.log(item / 100 * 10); // 14
+console.log(item - 14);
+//5
+console.log(Math.floor(23.87));
+
+//6
+console.log(Number.parseFloat('39.73pfkl'));
+
+//7
+console.log(Number.parseInt('39.73pfkl34'));
